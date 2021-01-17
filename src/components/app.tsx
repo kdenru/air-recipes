cef50fe010999fb9b4459a4133300aadbedeb6c1
kdenru <kdenru@gmail.com>
2021-01-17 20:50:47 +0700

    add material ui kit

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import { Container } from "@material-ui/core";
 
 import "./app.styl";
 
 const App: React.FunctionComponent = () => {
-  return <div className="appContainer">hello</div>;
+  return <Container maxWidth="lg">hello</Container>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import App from "./app";
+import App from "./components/app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production" && module.hot) {
   module.hot.accept();
 }
