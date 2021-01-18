9600be0e0979e534cec8cf13180a38353651fd19
kdenru <kdenru@gmail.com>
2021-01-18 11:59:13 +0700

    resolve paths

--- webpack.common.ts
+++ webpack.common.ts
@@ -1,8 +1,11 @@
 import * as webpack from "webpack";
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
 
 const config: webpack.Configuration = {
   resolve: {
     extensions: [".ts", ".tsx", ".js"],
+    // @ts-ignore https://github.com/dividab/tsconfig-paths-webpack-plugin/issues/61
+    plugins: [new TsconfigPathsPlugin()],
   },
   module: {
     rules: [
